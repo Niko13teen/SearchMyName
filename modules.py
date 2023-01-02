42a788f3db9360fdbc21f804fc75cd0be8bcfb34
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2023-01-03 01:20:15 +0700

    Create modules.py

---
+++ modules.py
@@ -0,0 +1,27 @@
+from requests_html import HTMLSession
+from colorama import init, Fore
+
+def popular_services(username):
+	session = HTMLSession()
+	
+	def telegram(username):
+		service = 'https://t.me/'
+		response = session.get(service + username)
+		result = response.html.find('.tgme_page_extra', first=True)
+		if bool(result) != False:
+			print(Fore.GREEN + f"[+] Found: {service}{username}")
+		else:
+			print(Fore.RED + f"[-] Not found: {service}{username}")
+	
+	telegram(username)
+	
+	def youtube(username):
+    service = 'https://youtube.com/@'
+		response = session.get(service + username)
+		if response.status_code == 200:
+			print(Fore.GREEN + f"[+] Found: {service}{username}")
+		else:
+			print(Fore.RED + f"[-] Not found: {service}{username}")		
+		
+	youtube(username)
+
