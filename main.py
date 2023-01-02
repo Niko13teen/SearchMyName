735fa121daa34e6f0b73e078aadfbb046eaa5e7b
Sergey Rusinov <70644336+Niko13teen@users.noreply.github.com>
2023-01-02 22:01:41 +0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,27 @@
+import requests
+import argparse
+import services
+from colorama import init, Fore
+
+
+def main(username: str):
+	services.init()
+	print(f"\n\n[*] All Services: {len(services.service)}\n[?] TG Author: https://t.me/niko13teen\n\n")
+	for lists in services.service:
+		try:
+			response = requests.get(lists + username)
+			if response.status_code == 200:
+				print(Fore.GREEN + f"[+] Found: {lists}{username}")
+			else:
+				print(Fore.RED + f"[-] Not found: {lists}")
+		except Exception as error:
+			pass
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--username', '-u',
+                        action='store',
+                        dest='username',
+                        help='write username')
+    args = parser.parse_args()
+    main(args.username)
